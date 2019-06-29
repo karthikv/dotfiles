299d352c6f55a95633bced64b495057b7c66c51a
Karthik Viswanathan <karthik.ksv@gmail.com>
2019-06-28 17:06:23 -0700

    Fix install script and remove unused files.

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 cd ~
 echo "Cloning dotfiles repository..."
-git clone --recursive https://github.com/karthikv/dotfiles.git
+git clone git@github.com:karthikv/dotfiles.git
 
 echo "Symlinking dotfiles..."
 ln -s ~/dotfiles/vimrc .vimrc
-ln -s ~/dotfiles/zshrc .zshrc
 ln -s ~/dotfiles/vim .vim
-ln -s ~/dotfiles/oh-my-zsh .oh-my-zsh
 
 cd ~
 echo "Done!"
