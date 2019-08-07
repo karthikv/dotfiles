4e71c5ee65814cae2a80316dc81b683531f83fe2
Karthik Viswanathan <karthik.ksv@gmail.com>
2019-08-07 01:23:20 -0700

    Symlink .bashrc in install script.

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ git clone git@github.com:karthikv/dotfiles.git
 echo "Symlinking dotfiles..."
 ln -s ~/dotfiles/vimrc .vimrc
 ln -s ~/dotfiles/vim .vim
+ln -s ~/dotfiles/bashrc .bashrc
 
-cd ~
 echo "Done!"
